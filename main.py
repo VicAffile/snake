34b3aacfa852f5b9538dfa8093417c09dfc35ca9
VicAffile <victoraffile@gmail.com>
2021-09-30 19:16:39 +0200

    Afficher et bouger la tête du serpent, et affichage des bords

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ hauteur_ecran = 600
 
 class Jeu:
 
-
     def __init__(self):
 
         self.ecran = pygame.display.set_mode((largeur_ecran, hauteur_ecran))
 
         self.joue = True
 
+        self.serpent = [300, 300]
+        self.direction = [0, 0]
+        self.taille = 10
 
     def boucle(self):
 
@@ -23,16 +25,54 @@ class Jeu:
 
             for evenement in pygame.event.get():
 
+                '''keystate = pygame.key.get_pressed()
+                if keystate[pygame.K_LEFT]:
+                    self.direction = [-10, 0]
+                if keystate[pygame.K_RIGHT]:
+                    self.direction = [10, 0]
+                if keystate[pygame.K_UP]:
+                    self.direction = [0, 10]
+                if keystate[pygame.K_DOWN]:
+                    self.direction = [0, -10]'''
+
                 if evenement.type == pygame.QUIT:
                     sys.exit()
 
+                if evenement.type == pygame.KEYDOWN:
+
+                    if evenement.key == pygame.K_UP:
+                        self.direction = [0, -10]
+
+                    if evenement.key == pygame.K_RIGHT:
+                        self.direction = [10, 0]
+
+                    if evenement.key == pygame.K_LEFT:
+                        self.direction = [-10, 0]
+
+                    if evenement.key == pygame.K_DOWN:
+                        self.direction = [0, 10]
+
+                
+                
+            self.serpent[0] += self.direction[0]
+            self.serpent[1] += self.direction[1]
+
+            self.direction = [0, 0]
 
             self.ecran.fill((0, 0, 0))
+
+            self.bordure()
+
+            pygame.draw.rect(self.ecran, (0, 255, 0), (self.serpent[0], self.serpent[1], self.taille, self.taille))
+
             pygame.display.flip()
 
 
-if __name__ == '__main__':
+    def bordure(self):
+        pygame.draw.rect(self.ecran, (255, 255, 255), (100, 50, 600, 500), 3)
 
+
+if __name__ == '__main__':
     pygame.init()
     Jeu().boucle()
     pygame.quit()
