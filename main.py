c2ffe95309fbfc66d8e6355476d0ab5999eb8cfb
VicAffile <victoraffile@gmail.com>
2021-10-04 11:02:17 +0200

    Avancer le serpent à une vitesse adapté

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ pygame.display.set_caption('Snake')
 largeur_ecran = 800
 hauteur_ecran = 600
 
-
 class Jeu:
 
     def __init__(self):
@@ -15,25 +14,18 @@ class Jeu:
 
         self.joue = True
 
-        self.serpent = [300, 300]
-        self.direction = [0, 0]
+        self.tete = [300, 300]
+        self.direction = [10, 0]
         self.taille = 10
+
+        self.temps_boucle = pygame.time.get_ticks()
+
 
     def boucle(self):
 
         while self.joue:
 
             for evenement in pygame.event.get():
-
-                '''keystate = pygame.key.get_pressed()
-                if keystate[pygame.K_LEFT]:
-                    self.direction = [-10, 0]
-                if keystate[pygame.K_RIGHT]:
-                    self.direction = [10, 0]
-                if keystate[pygame.K_UP]:
-                    self.direction = [0, 10]
-                if keystate[pygame.K_DOWN]:
-                    self.direction = [0, -10]'''
 
                 if evenement.type == pygame.QUIT:
                     sys.exit()
@@ -52,21 +44,20 @@ class Jeu:
                     if evenement.key == pygame.K_DOWN:
                         self.direction = [0, 10]
 
-                
-                
-            self.serpent[0] += self.direction[0]
-            self.serpent[1] += self.direction[1]
 
-            self.direction = [0, 0]
+            if (pygame.time.get_ticks() - self.temps_boucle) >= 200:
+                self.tete[0] += self.direction[0]
+                self.tete[1] += self.direction[1]
+                self.temps_boucle = pygame.time.get_ticks()
+
 
             self.ecran.fill((0, 0, 0))
 
             self.bordure()
 
-            pygame.draw.rect(self.ecran, (0, 255, 0), (self.serpent[0], self.serpent[1], self.taille, self.taille))
+            pygame.draw.rect(self.ecran, (0, 255, 0), (self.tete[0], self.tete[1], self.taille, self.taille))
 
-
-            if self.serpent[0] <= 100 or self.serpent[0] >= 700 or self.serpent[1] <= 50 or self.serpent[1] >= 550:
+            if self.tete[0] <= 100 or self.tete[0] >= 700 or self.tete[1] <= 50 or self.tete[1] >= 550:
                 sys.exit()
 
             pygame.display.flip()
