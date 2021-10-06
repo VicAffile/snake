c77499ac9da11422606cfbcacbd7c20b9db7d0fa
VicAffile <victoraffile@gmail.com>
2021-10-06 23:19:19 +0200

    Afficher le corp du serpent et augmenter sa taille en mangeant une pomme

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.display.set_caption('Snake')
 
 largeur_ecran = 800
 hauteur_ecran = 600
+marge_horizontal = 100
+marge_vertical = 50
 
 class Jeu:
 
@@ -14,12 +16,17 @@ class Jeu:
 
         self.joue = True
 
-        self.serpent_tete = [300, 300]
-        self.serpent_direction = [10, 0]
-
-        self.pomme = [random.randrange(110, 690, 10), random.randrange(60, 540, 10)]
-
         self.taille = 10
+
+        self.serpent_tete = [300, 300]
+        self.serpent_position = []
+        self.serpent_position.append([int(self.serpent_tete[0]), int(self.serpent_tete[1])])
+        self.serpent_position.append([self.serpent_tete[0] - self.taille, self.serpent_tete[1]])
+        self.serpent_position.append([self.serpent_tete[0] - 2 * self.taille, self.serpent_tete[1]])
+        self.serpent_direction = [10, 0]
+        self.serpent_longueur = 3
+
+        self.pomme = [random.randrange((marge_horizontal + self.taille), (largeur_ecran - marge_horizontal - self.taille), 10), random.randrange((marge_vertical + self.taille), (hauteur_ecran - marge_vertical - self.taille), 10)]
 
         self.temps_boucle = pygame.time.Clock()
 
@@ -50,17 +57,20 @@ class Jeu:
 
             self.serpent_tete[0] += self.serpent_direction[0]
             self.serpent_tete[1] += self.serpent_direction[1]
+            self.serpent_position.insert(0, [int(self.serpent_tete[0]), int(self.serpent_tete[1])])
+            if len(self.serpent_position) >= self.serpent_longueur:
+                self.serpent_position.pop()
 
             if self.pomme[0] == self.serpent_tete[0] and self.pomme[1] == self.serpent_tete[1]:
-                self.pomme = [random.randrange(110, 690, 10), random.randrange(60, 540, 10)]
-
+                self.serpent_longueur += 1
+                self.pomme = [random.randrange((marge_horizontal + self.taille), (largeur_ecran - marge_horizontal - self.taille), 10), random.randrange((marge_vertical + self.taille), (hauteur_ecran - marge_vertical - self.taille), 10)]
 
             self.ecran.fill((0, 0, 0))
 
             self.bordure()
-            self.temps_boucle.tick(20)
+            self.temps_boucle.tick(15)
 
-            pygame.draw.rect(self.ecran, (0, 255, 0), (self.serpent_tete[0], self.serpent_tete[1], self.taille, self.taille))
+            self.generer_serpent()
 
             pygame.draw.rect(self.ecran, (255, 0, 0), (self.pomme[0], self.pomme[1], self.taille, self.taille))
 
@@ -71,7 +81,12 @@ class Jeu:
 
 
     def bordure(self):
-        pygame.draw.rect(self.ecran, (255, 255, 255), (100, 50, 600, 500), 3)
+        pygame.draw.rect(self.ecran, (255, 255, 255), (marge_horizontal, marge_vertical, (largeur_ecran - 2 * marge_horizontal), (hauteur_ecran - 2 * marge_vertical)), 3)
+
+
+    def generer_serpent(self):
+        for partie_serpent in self.serpent_position:
+            pygame.draw.rect(self.ecran, (0, 255, 0), (partie_serpent[0], partie_serpent[1], self.taille, self.taille))
 
 
 if __name__ == '__main__':
