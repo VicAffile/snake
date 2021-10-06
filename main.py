ac4e16b935775a71018566712ec1c0956650d712
VicAffile <victoraffile@gmail.com>
2021-10-07 00:12:29 +0200

    Création de la classe Serpent : Code plus propre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys, random
 import pygame
 
+from classe.serpent import Serpent
+
 pygame.display.set_caption('Snake')
 
 largeur_ecran = 800
@@ -18,15 +20,18 @@ class Jeu:
 
         self.taille = 10
 
-        self.serpent_tete = [300, 300]
-        self.serpent_position = []
-        self.serpent_position.append([int(self.serpent_tete[0]), int(self.serpent_tete[1])])
-        self.serpent_position.append([self.serpent_tete[0] - self.taille, self.serpent_tete[1]])
-        self.serpent_position.append([self.serpent_tete[0] - 2 * self.taille, self.serpent_tete[1]])
-        self.serpent_direction = [10, 0]
-        self.serpent_longueur = 3
+        self.serpent = Serpent()
+        #self.serpent_tete = [300, 300]
+        #self.serpent_position = []
+        #self.serpent_position.append([int(self.serpent_tete[0]), int(self.serpent_tete[1])])
+        #self.serpent_position.append([self.serpent_tete[0] - self.taille, self.serpent_tete[1]])
+        #self.serpent_position.append([self.serpent_tete[0] - 2 * self.taille, self.serpent_tete[1]])
+        #self.serpent_direction = [10, 0]
+        #self.serpent_longueur = 3
 
         self.pomme = [random.randrange((marge_horizontal + self.taille), (largeur_ecran - marge_horizontal - self.taille), 10), random.randrange((marge_vertical + self.taille), (hauteur_ecran - marge_vertical - self.taille), 10)]
+
+        self.score = 0
 
         self.temps_boucle = pygame.time.Clock()
 
@@ -42,63 +47,53 @@ class Jeu:
 
                 if evenement.type == pygame.KEYDOWN:
 
-                    if evenement.key == pygame.K_UP and self.serpent_direction != [0, 10]:
-                            self.serpent_direction = [0, -10]
+                    if evenement.key == pygame.K_UP and self.serpent.direction != [0, 10]:
+                            self.serpent.direction = [0, -10]
 
-                    if evenement.key == pygame.K_RIGHT and self.serpent_direction != [-10, 0]:
-                            self.serpent_direction = [10, 0]
+                    if evenement.key == pygame.K_RIGHT and self.serpent.direction != [-10, 0]:
+                            self.serpent.direction = [10, 0]
 
-                    if evenement.key == pygame.K_LEFT and self.serpent_direction != [10, 0]:
-                            self.serpent_direction = [-10, 0]
+                    if evenement.key == pygame.K_LEFT and self.serpent.direction != [10, 0]:
+                            self.serpent.direction = [-10, 0]
 
-                    if evenement.key == pygame.K_DOWN and self.serpent_direction != [0, -10]:
-                            self.serpent_direction = [0, 10]
-
-
-            self.serpent_tete[0] += self.serpent_direction[0]
-            self.serpent_tete[1] += self.serpent_direction[1]
-            self.serpent_position.insert(0, [int(self.serpent_tete[0]), int(self.serpent_tete[1])])
-            if len(self.serpent_position) >= self.serpent_longueur:
-                self.serpent_position.pop()
+                    if evenement.key == pygame.K_DOWN and self.serpent.direction != [0, -10]:
+                            self.serpent.direction = [0, 10]
 
 
-            self.mordre_queue()
+            self.serpent.avancer()
 
 
-            if self.pomme[0] == self.serpent_tete[0] and self.pomme[1] == self.serpent_tete[1]:
-                self.serpent_longueur += 1
+            self.serpent.mordre_queue()
+
+
+            if self.pomme[0] == self.serpent.tete[0] and self.pomme[1] == self.serpent.tete[1]:
+                self.serpent.longueur += 1
+                self.score += 1
                 self.pomme = [random.randrange((marge_horizontal + self.taille), (largeur_ecran - marge_horizontal - self.taille), 10), random.randrange((marge_vertical + self.taille), (hauteur_ecran - marge_vertical - self.taille), 10)]
 
             self.ecran.fill((0, 0, 0))
 
-            self.bordure()
-            self.temps_boucle.tick(15)
+            self.bordures()
 
-            self.generer_serpent()
+            self.serpent.generer(self.ecran)
 
             pygame.draw.rect(self.ecran, (255, 0, 0), (self.pomme[0], self.pomme[1], self.taille, self.taille))
 
-            if self.serpent_tete[0] <= 95 or self.serpent_tete[0] >= 700 or self.serpent_tete[1] <= 45 or self.serpent_tete[1] >= 550:
-                sys.exit()
+            self.sortie_bordures()
+
+            self.temps_boucle.tick(15)
 
             pygame.display.flip()
 
 
-    def bordure(self):
+    def bordures(self):
         pygame.draw.rect(self.ecran, (255, 255, 255), (marge_horizontal, marge_vertical, (largeur_ecran - 2 * marge_horizontal), (hauteur_ecran - 2 * marge_vertical)), 3)
 
 
-    def generer_serpent(self):
-        for partie_serpent in self.serpent_position:
-            pygame.draw.rect(self.ecran, (0, 255, 0), (partie_serpent[0], partie_serpent[1], self.taille, self.taille))
+    def sortie_bordures(self):
+        if self.serpent.tete[0] <= (marge_horizontal - 5) or self.serpent.tete[0] >= (largeur_ecran - marge_horizontal) or self.serpent.tete[1] <= (marge_vertical - 5) or self.serpent.tete[1] >= (hauteur_ecran - marge_vertical):
+            sys.exit()
 
-
-    def mordre_queue(self):
-        del self.serpent_position[0]
-        for partie_serpent in self.serpent_position:
-            if partie_serpent == self.serpent_tete:
-                sys.exit()
-        self.serpent_position.insert(0, [int(self.serpent_tete[0]), int(self.serpent_tete[1])])
 
 
 if __name__ == '__main__':
