a66cafe0e53cf398ee351d35e2f0caab77bad9b0
VicAffile <victoraffile@gmail.com>
2021-10-11 14:56:44 +0200

    Jeu en objet

--- classe/menu.py
+++ classe/menu.py
@@ -25,10 +25,7 @@ class Menu:
 
             ecran.fill((0, 0, 0))
 
-            self.afficher_message(titre.upper(), 'grande', (255, 255, 255), ((largeur_ecran / 2 - 50), (hauteur_ecran / 2 - 250), 100, 50), ecran)
-            self.afficher_message("Le but du jeu est de manger le plus de pommes possible", 'petite', (240, 240, 240), ((largeur_ecran / 2 - 180), (hauteur_ecran / 2 - 100), 200, 5), ecran)
-            self.afficher_message( "Le serpent grossira à chaque pomme manger, s'il touche le bord ou se mord la queue c'est perdu", 'petite', (240, 240, 240), ((largeur_ecran / 2 - 300), (hauteur_ecran / 2 - 80), 200, 50), ecran)
-            self.afficher_message("Appuyer sur Entrée pour jouer", 'moyenne', (255, 255, 255), ((largeur_ecran / 2 - 150), (hauteur_ecran / 2 + 150), 200, 5), ecran)
+            self.afficher_messages(ecran, titre, largeur_ecran, hauteur_ecran)
 
             pygame.display.flip()
 
@@ -45,3 +42,10 @@ class Menu:
         message = taille.render(message, True, couleur)
 
         ecran.blit(message, dimension)
+
+
+    def afficher_messages(self, ecran, titre, largeur_ecran, hauteur_ecran):
+            self.afficher_message(titre.upper(), 'grande', (255, 255, 255), ((largeur_ecran / 2 - 50), (hauteur_ecran / 2 - 250), 100, 50), ecran)
+            self.afficher_message("Le but du jeu est de manger le plus de pommes possible", 'petite', (240, 240, 240), ((largeur_ecran / 2 - 180), (hauteur_ecran / 2 - 100), 200, 5), ecran)
+            self.afficher_message( "Le serpent grossira à chaque pomme manger, s'il touche le bord ou se mord la queue c'est perdu", 'petite', (240, 240, 240), ((largeur_ecran / 2 - 300), (hauteur_ecran / 2 - 80), 200, 50), ecran)
+            self.afficher_message("Appuyer sur Entrée pour jouer", 'moyenne', (255, 255, 255), ((largeur_ecran / 2 - 150), (hauteur_ecran / 2 + 150), 200, 5), ecran)
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import sys, random
 import pygame
 
 from classe.menu import Menu
-from classe.serpent import Serpent
-from classe.pomme import Pomme
+from classe.jeu import Jeu
 
 
 
 
-class Jeu:
+class Logiciel:
 
     def __init__(self):
         self.titre = 'Snake'
@@ -21,86 +19,21 @@ class Jeu:
 
         self.ecran = pygame.display.set_mode((self.largeur_ecran, self.hauteur_ecran))
         self.menu = Menu(True)
-        self.joue = False
-
-        self.taille = 10
-
-        self.serpent = Serpent()
-        self.pomme = Pomme(self.marge_horizontal, self.marge_vertical, self.largeur_ecran, self.hauteur_ecran, self.serpent.positions)
-
-        self.score = 0
-
-        self.temps_boucle = pygame.time.Clock()
+        self.jeu = Jeu(False, self.largeur_ecran, self.hauteur_ecran, self.marge_horizontal, self.marge_vertical)
 
 
     def boucle(self):
 
         if self.menu.actif == True:
             self.menu.boucle(self.ecran, self.titre, self.largeur_ecran, self.hauteur_ecran)
-            self.joue = True
+            self.jeu.actif = True
 
 
-        while self.joue:
-
-            for evenement in pygame.event.get():
-
-                if evenement.type == pygame.QUIT:
-                    sys.exit()
-
-                if evenement.type == pygame.KEYDOWN:
-
-                    if evenement.key == pygame.K_UP and self.serpent.direction != [0, 10]:
-                            self.serpent.direction = [0, -10]
-
-                    if evenement.key == pygame.K_RIGHT and self.serpent.direction != [-10, 0]:
-                            self.serpent.direction = [10, 0]
-
-                    if evenement.key == pygame.K_LEFT and self.serpent.direction != [10, 0]:
-                            self.serpent.direction = [-10, 0]
-
-                    if evenement.key == pygame.K_DOWN and self.serpent.direction != [0, -10]:
-                            self.serpent.direction = [0, 10]
-
-
-            self.serpent.avancer()
-
-            self.serpent.mordre_queue()
-
-            self.pomme_mange()
-
-            self.ecran.fill((0, 0, 0))
-
-            self.bordures()
-
-            self.serpent.afficher(self.ecran)
-
-            self.pomme.afficher(self.ecran)
-
-            self.sortie_bordures()
-
-            self.temps_boucle.tick(15)
-
-            pygame.display.flip()
-
-
-    def bordures(self):
-        pygame.draw.rect(self.ecran, (255, 255, 255), (self.marge_horizontal, self.marge_vertical, (self.largeur_ecran - 2 * self.marge_horizontal), (self.hauteur_ecran - 2 * self.marge_vertical)), 3)
-
-
-    def sortie_bordures(self):
-        if self.serpent.tete[0] <= (self.marge_horizontal - 5) or self.serpent.tete[0] >= (self.largeur_ecran - self.marge_horizontal) or self.serpent.tete[1] <= (self.marge_vertical - 5) or self.serpent.tete[1] >= (self.hauteur_ecran - self.marge_vertical):
-            sys.exit()
-
-
-    def pomme_mange(self):
-        if self.pomme.position[0] == self.serpent.tete[0] and self.pomme.position[1] == self.serpent.tete[1]:
-            self.serpent.longueur += 1
-            self.score += 1
-            self.pomme.position = self.pomme.generer(self.marge_horizontal, self.marge_vertical, self.largeur_ecran, self.hauteur_ecran, self.serpent.positions)
-
+        if self.jeu.actif == True:
+            self.jeu.boucle(self.ecran, self.largeur_ecran, self.hauteur_ecran, self.marge_horizontal, self.marge_vertical)
 
 
 if __name__ == '__main__':
     pygame.init()
-    Jeu().boucle()
+    Logiciel().boucle()
     pygame.quit()
